d50fcf92e37d70bf3cc743aaabf59f2544ac1c7a
PTHoa <18520765@gm.uit.edu.vn>
2022-04-17 12:27:53 +0700

    Ignition

---
+++ Ignition_Car.cpp
@@ -0,0 +1,19 @@
+Mục đích của hệ thống đánh lửa là tạo ra tuổi vôn rất cao từ pin 12 vôn của ô tô, 
+và lần lượt gửi nhiệt độ này đến từng bugi, đốt cháy hỗn hợp nhiên liệu-không khí trong buồng đốt của động cơ.
+Bugi (tiếng Pháp: Bougie, tiếng Anh: spark plug): Công cụ để nguồn điện phát ra tia lửa điện qua một khoảng trống (giống như tia sét). 
+Nguồn điện này phải có điện áp rất cao để tia lửa có thể phóng qua khoảng trống và tia lửa mạnh. 
+Thông thường, điện áp giữa hai cực của bugi khoảng từ 40.000 đến 100.000 volt. Một số xe đòi hỏi phải sử dụng loại bugi nóng. 
+Loại bugi này được thiết kế có chất sứ bao bọc tiếp xúc với kim loại ít hơn do vậy việc trao đổi nhiệt kém hơn và nến nóng hơn và làm sạch bụi bẩn tốt hơn. 
+Bugi lạnh thì ngược lại, thiết kế với vùng trao đổi nhiệt lớn hơn vì vậy sẽ nguội hơn khi hoạt động. 
+Động cơ hiệu suất cao sẽ sinh nhiều nhiệt hơn do vậy phải sử dụng bugi nguội hơn. Nếu bugi quá nóng, nó sẽ làm cho hỗn hợp cháy trước khi tia lửa phát ra.
+Bôbin: Là bộ phận sinh ra cao áp để tạo ra tia lửa. Điện thế cao được sinh ra do cảm ứng giữa hai cuộn dây. 
+Một cuộn có ít vòng được gọi là cuộn sơ cấp (màu vàng), cuốn xung quanh cuộn sơ cấp (màu đen) nhưng nhiều vòng hơn là cuộn thứ cấp. 
+Cuộn thứ cấp có số vòng lớn gấp hàng trăm lần cuộn sơ cấp. 
+Dòng điện từ nguồn điện chạy qua cuộn sơ cấp của bôbin, đột ngột, dòng điện bị ngắt đi tại thời điểm đánh lửa do má vít 
+(đang đóng kín mạch điện thì đột ngột mở ra). Khi dòng điện ở cuộn sơ cấp bị ngắt đi, từ trường điện do cuộn sơ cấp sinh ra giảm đột ngột. 
+Theo nguyên tắc cảm ứng điện từ, cuộn thứ cấp sinh ra một dòng điện để chống lại sự thay đổi từ trường đó. 
+Do số vòng của cuộn thứ cấp lớn gấp rất nhiều lần số vòng dây cuộn sơ cấp nên dòng điện ở cuộn thứ cấp có điện áp rất lớn (có thể đến 100.000 vôn). 
+Dòng điện cao áp này được bộ chia điện đưa đến nến bugi qua dây cao áp.
+Bộ chia điện: Chia nguồn điện cao áp từ bôbin đến các xi lanh. Điều này được thực hiện bởi trục bộ chia điện và con quay gắn ở đầu. 
+Cuộn thứ cấp của tăng điện được kết nối với con quay, nắp bộ chia điện có các đầu nối với các dây cao áp đến các xi lanh. 
+Khi con quay quay vòng tròn nó sẽ chia nguồn điện cao áp cho các xi lanh theo một tứ tự nhất định.
